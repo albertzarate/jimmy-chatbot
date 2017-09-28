63f70c679945d9ad33e830019ccd877fa719bb59
ZhongJLiang <zhonglin.liang@sjsu.edu>
2017-09-29 06:45:05 +0800

    initing chatbot

--- testing.js
+++ testing.js
@@ -1,1 +1,10 @@
-console.log($_get("hub.challenge"));
+var sys = require("sys") , my_http = require("http");
+my_http.createServer(function(request,response){
+    sys.put("I got kicked");
+    response.writeHeader(200,{"Content-Type": "text/plain"});
+    response.write("Hello world");
+    response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080");
+
+my_http.createServer(function(request,response){}).listen(8080);
