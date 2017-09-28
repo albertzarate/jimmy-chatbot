2f4cb2b8b598d2293eacaa2668cc46630379bd86
ZhongJLiang <zhonglin.liang@sjsu.edu>
2017-09-29 07:15:11 +0800

    edited

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/', function(req, res){
-    res.send("Hi I am a chatbot")
-})
-
 //Facebook authentication 
 
 app.get('/webhook/', function(req,res){
