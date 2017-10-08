c3f11e3d9b19b9052cb34aba33332755b05e884a
ZhongJLiang <zhonglin.liang@sjsu.edu>
2017-10-08 13:11:45 +0800

    get_announcement function basic structure

--- jimmy_ai.py
+++ jimmy_ai.py
@@ -3,12 +3,16 @@ from itty import *
 import json
 import pprint
 import jimmy 
+import requests
 
-
+# api info website for Canvas https://canvas.instructure.com/courses/785215
+# api documentation for Canvas https://canvas.instructure.com/doc/api/index.html
+# general format for requesting url: https://canvas.instructure.com/api/v1/ + path
+# example: https://canvas.instructure.com/api/v1/courses?include[]=total_scores  for the request for user's grades
 
 @post('/')
 def index(request):
-	r = json.loads(request.body)
+	global r = json.loads(request.body)
 	if not r:
 	    return "True"
 
@@ -20,8 +24,29 @@ def index(request):
 	if action == 'get_grades':
 		get_grades()
 
+	if action == 'get_announcement':
+    	get_announcement()
+
+def get_announcement():
+	url = 'https://canvas.instructure.com/api/v1//api/v1/announcements'
+	params = dict(
+		context_codes= ''
+		start_date = ''
+		end_date = ''
+		active_only = ''
+	)
+	r = requests.get(url, params,headers = {"Authorization":access_token} )
+	data = json.loads(r.text)
+	title = data['title'], message = data['message']
+	output = 'reply message'
+
+def Authorization():
+    	global access_token
+
+
 
 
 if __name__ == "__main__": 
 
 	run_itty(server='wsgiref', host='0.0.0.0', port=8080)
+	
